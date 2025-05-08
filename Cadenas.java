843f34f7f0bbfca3b6404add8fc1dba59792b3c9
BryanVenegas <20243rd060@utez.edu.mx>
2025-05-08 11:44:32 -0600

    ahi ta el codigo

---
+++ Cadenas.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Cadenas {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Introduce tu nombre: ");
+    String nombre = scanner.nextLine();
+    System.out.print("Introduce tu apellido: ");
+    String apellido = scanner.nextLine();
+    System.out.print("tu nombre es: "+ nombre + " y tu apellido es: " + apellido + "\n");
+}
+
+
+    
+}
