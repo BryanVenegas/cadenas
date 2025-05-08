023a950eb9c9efa50c25c68df5ed3e0f184face7
BryanVenegas <20243rd060@utez.edu.mx>
2025-05-08 11:53:00 -0600

    agrege los apellidos

--- Cadenas.java
+++ Cadenas.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 
 public class Cadenas {
 public static void main(String[] args) {
+    // Ejercicio 1: Crear un programa que pida al usuario su nombre y apellido y los imprima en la consola.
     Scanner scanner = new Scanner(System.in);
     System.out.print("Introduce tu nombre: ");
     String nombre = scanner.nextLine();
     System.out.print("Introduce tu apellido: ");
     String apellido = scanner.nextLine();
-    System.out.print("tu nombre es: "+ nombre + " y tu apellido es: " + apellido + "\n");
+    System.out.print("Introduce tu apellido parterno: ");
+    String apellido2 = scanner.nextLine();
+    System.out.print("tu nombre es: "+ nombre + " " + apellido + " " + apellido2 +"\n");
 }
 
 
